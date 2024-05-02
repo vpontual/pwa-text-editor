ef3d2f7ab1661f0a6b192405676179d4ec243089
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-01 20:20:09 -0400

    updated title in webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "Contact Cards",
+        title: "J.A.T.E. - Just Another Text Editor",
       }),
       new InjectManifest({
         swSrc: "./src-sw.js",
