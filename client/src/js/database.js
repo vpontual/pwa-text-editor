33193d025c4522e47c6f3742e9646f7d1a662f62
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-02 18:35:11 -0400

    updated the put route again to try and save the data

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,7 +18,7 @@ export const putDb = async (content) => {
     const jateDb = await openDB("jate", 1);
     const tx = jateDb.transaction("jate", "readwrite");
     const store = tx.objectStore("jate");
-    const request = store.put({ content: clonedContent });
+    const request = store.put(content);
     const result = await request;
     console.log("Data saved to the database", result);
   } catch (error) {
